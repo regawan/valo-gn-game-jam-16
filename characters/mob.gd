b77e0ff015e910f6d8c1405f8f00ce9cfb6763d6
regawan <andreaswager99@gmail.com>
2025-03-04 00:44:35 +0200

    Mob init done.

--- characters/mob.gd
+++ characters/mob.gd
@@ -10,3 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
