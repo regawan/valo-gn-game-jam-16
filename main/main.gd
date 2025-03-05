15f753d83243c094f10cdd9f7f6f826dfb84f9ad
regawan <andreaswager99@gmail.com>
2025-03-06 01:14:14 +0200

    Darkness and pointlight added.

--- main/main.gd
+++ main/main.gd
@@ -53,7 +53,7 @@ func _on_mob_timer_timeout():
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	var velocity = Vector2(randf_range(100.0, 150.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
