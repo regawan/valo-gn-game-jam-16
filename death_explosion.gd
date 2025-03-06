85f42837f022a3a75283c37bcf4ba12c36171e7b
regawan <andreaswager99@gmail.com>
2025-03-06 23:52:57 +0200

    Zombie exploding on death completed.

--- death_explosion.gd
+++ death_explosion.gd
@@ -1,1 +1,12 @@
 extends CPUParticles2D
+
+func _ready() -> void:
+    emitting = true
+    $DeathLight.energy = 1.0
+
+func _process(_delta: float) -> void:
+    $DeathLight.energy -= 0.01
+
+func _on_finished() -> void:
+    queue_free()
+
