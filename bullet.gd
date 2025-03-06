c0d0630f9d5a84969685cfc1ad43075675843d5a
regawan <andreaswager99@gmail.com>
2025-03-06 21:13:17 +0200

    Bullet Tracer init.

--- bullet.gd
+++ bullet.gd
@@ -2,18 +2,22 @@ extends Area2D
 
 var travelled_distance = 0
 const SPEED = 1000
-const RANGE = 1200
+const RANGE = 300
+
 
 func _physics_process(delta:float):
-    var direction =  Vector2.RIGHT.rotated(rotation)
-    position += direction * SPEED * delta
+	var direction =  Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta
 
-    travelled_distance += SPEED * delta
-    if travelled_distance > RANGE:
-        queue_free()
+	travelled_distance += SPEED * delta
+	if travelled_distance > RANGE:
+		queue_free()
+
+	$Tracer.texture_scale = max(0, 1.0 - travelled_distance / RANGE)
+	$Tracer.energy = max(0.1, 1.0 - travelled_distance / RANGE)
 
 
 func _on_body_entered(body:Node2D):
-    queue_free()
-    if body.has_method("take_damage"):
-        body.take_damage()
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
--- gun.gd
+++ gun.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
 signal shot
+var can_shoot = true
+
 
 func _physics_process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
 
 
 func shoot() -> void:
@@ -13,7 +17,9 @@ func shoot() -> void:
 	new_bullet.rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
 	shot.emit()
+	can_shoot = false
+	$ShotTimer.start()
 
 
-func _on_timer_timeout() -> void:
-	shoot()
+func _on_shot_timer_timeout() -> void:
+	can_shoot = true
